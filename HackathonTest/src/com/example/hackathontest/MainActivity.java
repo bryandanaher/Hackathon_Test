faae7c9f065acde71b27b1c9a0a0ad6cf07cf241
Bryan <bryandanhaer@gmail.com>
2014-07-16 19:50:01 -0500

    got it working. had to add a line to the androidmanifest to give the app permission to use the internet

--- HackathonTest/src/com/example/hackathontest/MainActivity.java
+++ HackathonTest/src/com/example/hackathontest/MainActivity.java
@@ -1,65 +1,201 @@
 package com.example.hackathontest;
+//
+//import android.support.v7.app.ActionBarActivity;
+//import android.support.v7.app.ActionBar;
+//import android.support.v4.app.Fragment;
+//import android.os.Bundle;
+//import android.view.LayoutInflater;
+//import android.view.Menu;
+//import android.view.MenuItem;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.os.Build;
+//
+//public class MainActivity extends ActionBarActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        if (savedInstanceState == null) {
+//            getSupportFragmentManager().beginTransaction()
+//                    .add(R.id.container, new PlaceholderFragment())
+//                    .commit();
+//        }
+//    }
+//
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
+//
+//    /**
+//     * A placeholder fragment containing a simple view.
+//     */
+//    public static class PlaceholderFragment extends Fragment {
+//
+//        public PlaceholderFragment() {
+//        }
+//
+//        @Override
+//        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                Bundle savedInstanceState) {
+//            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+//            return rootView;
+//        }
+//    }
+//
+//}
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+//import android.R;
+import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
-public class MainActivity extends ActionBarActivity {
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+public class MainActivity extends Activity
+{
+    // the Rotten Tomatoes API key of your application! get this from their website
+    private static final String API_KEY = "qzm44antn5p2ddbatv2haan9"; //<your api key!>; 
+
+    // the number of movies you want to get in a single request to their web server
+    private static final int MOVIE_PAGE_LIMIT = 10; 
+
+    private EditText searchBox;
+    private Button searchButton;
+    private ListView moviesList; 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);  
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
+        searchBox = (EditText) findViewById(R.id.text_search_box);
+        searchButton = (Button) findViewById(R.id.button_search);
+        searchButton.setOnClickListener(new OnClickListener()
+        {
+            // send an API request when the button is pressed
+            @Override
+            public void onClick(View arg0)
+            {
+                new RequestTask().execute("http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=" + API_KEY + "&q=" + searchBox.getText().toString().trim() + "&page_limit=" + MOVIE_PAGE_LIMIT);
+            }
+        });
+        moviesList = (ListView) findViewById(R.id.list_movies);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    private void refreshMoviesList(String[] movieTitles)
+    {
+        moviesList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, movieTitles));
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
+    private class RequestTask extends AsyncTask<String, String, String>
+    {
+        // make a request to the specified url
         @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
+        protected String doInBackground(String... uri)
+        {
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpResponse response;
+            String responseString = null;
+            try
+            {
+                // make a HTTP request
+                response = httpclient.execute(new HttpGet(uri[0]));
+                StatusLine statusLine = response.getStatusLine();
+                if (statusLine.getStatusCode() == HttpStatus.SC_OK)
+                {
+                    // request successful - read the response and close the connection
+                    ByteArrayOutputStream out = new ByteArrayOutputStream();
+                    response.getEntity().writeTo(out);
+                    out.close();
+                    responseString = out.toString();
+                }
+                else
+                {
+                    // request failed - close the connection
+                    response.getEntity().getContent().close();
+                    throw new IOException(statusLine.getReasonPhrase());
+                }
+            }
+            catch (Exception e)
+            {
+                Log.d("Test", "Couldn't make a successful request!");
+            }
+            return responseString;
+        }
+
+        // if the request above completed successfully, this method will 
+        // automatically run so you can do something with the response
+        @Override
+        protected void onPostExecute(String response)
+        {
+            super.onPostExecute(response);
+
+            if (response != null)
+            {
+                try
+                {
+                    // convert the String response to a JSON object,
+                    // because JSON is the response format Rotten Tomatoes uses
+                    JSONObject jsonResponse = new JSONObject(response);
+
+                    // fetch the array of movies in the response
+                    JSONArray movies = jsonResponse.getJSONArray("movies");
+
+                    // add each movie's title to an array
+                    String[] movieTitles = new String[movies.length()];
+                    for (int i = 0; i < movies.length(); i++)
+                    {
+                        JSONObject movie = movies.getJSONObject(i);
+                        movieTitles[i] = movie.getString("title");
+                    }
+
+                    // update the UI
+                    refreshMoviesList(movieTitles);
+                }
+                catch (JSONException e)
+                {
+                    Log.d("Test", "Failed to parse the JSON response!");
+                }
+            }
         }
     }
-
 }
